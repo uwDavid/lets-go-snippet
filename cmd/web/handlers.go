a978f54d9f7312f3a08185ab25aa49c774a81fe2
uwDavid <uwDavid@users.noreply.github.com>
2023-09-25 18:08:29 -0400

    ch7-restful routing

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -8,10 +8,13 @@ import (
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		app.notFound(w) // use notFound() helper
-		return
-	}
+	// pat takes care of exact match
+	/*
+		if r.URL.Path != "/" {
+			app.notFound(w) // use notFound() helper
+			return
+		}
+	*/
 
 	s, err := app.snippets.Latest()
 	if err != nil {
@@ -22,7 +25,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	id, err := strconv.Atoi(r.URL.Query().Get(":id")) // pat doesn't strip ":"
 	if err != nil || id < 1 {
 		app.notFound(w) // use notFound() helper
 		return
@@ -40,13 +43,11 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	app.render(w, r, "show.page.tmpl", &templateData{Snippet: s})
 }
 
-func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		w.Header().Set("Allow", "POST")
-		app.clientError(w, http.StatusMethodNotAllowed) //clientError() helper
-		return
-	}
+func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet"))
+}
 
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	// to remove later
 	title := "O snail"
 	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n- Kobayashi"
@@ -57,5 +58,5 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 		app.serverError(w, err)
 		return
 	}
-	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/%d", id), http.StatusSeeOther)
 }
